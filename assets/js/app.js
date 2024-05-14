bf53071e123aec6c7d9bb55b2855eaf18b05c94e
Sandeep-508 <elsenothing321@gmail.com>
2024-05-14 23:30:54 +0530

    how to buy section is remaining

--- assets/js/app.js
+++ assets/js/app.js
@@ -52,15 +52,26 @@ menuicon.addEventListener("click", () => {
 // accoridon
 
 let accordion_item = document.querySelectorAll(".accoridon_items");
+let accordion_item_1 = document.querySelector(".accoridon_items");
 let svg_icon = document.querySelector(".svg_icon");
 let disp_text = document.querySelector(".disp_text");
+let svg_arrow = document.querySelector(".svg_fill");
+let text_1 = document.querySelector(".color_change");
+let text_2 = document.querySelector(".color_change_2");
 disp_text.style.display = "flex";
 svg_icon.style.transform = "rotate(180deg)";
+accordion_item_1.classList.add("bordr_change");
+svg_arrow.classList.add("fill_change");
+text_1.classList.add("text_change");
+text_2.classList.add("text_change");
 
 accordion_item.forEach(otherElement => {
     let svg_icon = otherElement.querySelector(".svg_icon");
     let disp_text = otherElement.querySelector(".disp_text");
     let clickBtn = otherElement.querySelector(".click_btn");
+    let text_1 = otherElement.querySelector(".color_change");
+    let text_2 = otherElement.querySelector(".color_change_2");
+    let svg_arrow = otherElement.querySelector(".svg_fill");
     clickBtn.addEventListener("click", () => {
         accordion_item.forEach(itm => {
             if (itm !== otherElement) {
@@ -68,16 +79,52 @@ accordion_item.forEach(otherElement => {
                 let disp_text = itm.querySelector(".disp_text");
                 disp_text.style.display = "none";
                 svg_icon.style.transform = "rotate(0deg)";
+                let text_1 = itm.querySelector(".color_change");
+                let text_2 = itm.querySelector(".color_change_2");
+                let svg_arrow = itm.querySelector(".svg_fill");
+                itm.classList.remove("bordr_change");
+                text_1.classList.remove("text_change");
+                text_2.classList.remove("text_change");
+                svg_arrow.classList.remove("fill_change");
             }
         });
         let text_prop = window.getComputedStyle(disp_text).display;
         if (text_prop === "none") {
             disp_text.style.display = "flex";
             svg_icon.style.transform = "rotate(180deg)";
+            otherElement.classList.add("bordr_change");
+            text_1.classList.add("text_change");
+            text_2.classList.add("text_change");
+            svg_arrow.classList.add("fill_change");
         } else {
             disp_text.style.display = "none";
             svg_icon.style.transform = "rotate(0deg)";
+            otherElement.classList.remove("bordr_change");
+            text_1.classList.remove("text_change");
+            text_2.classList.remove("text_change");
+            svg_arrow.classList.remove("fill_change");
         }
     });
 
+});
+
+
+// scroll to top
+
+let scroll_btn = document.querySelector(".scroll_top");
+let scroll_pt = 200;
+
+scroll_btn.classList.add("d-none");
+window.addEventListener("scroll", () => {
+    if (window.scrollY > scroll_pt) {
+        scroll_btn.classList.remove("d-none");
+    } else {
+        scroll_btn.classList.add("d-none");
+    }
+    scroll_btn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    });
 });
